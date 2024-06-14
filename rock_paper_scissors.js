8f3b6fcf444f3c25188b5ab10749c73d201df8ff
Tony <anthony.duncan117@gmail.com>
2024-06-14 16:58:33 -0400

    Add new function to return valid player choice

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,12 +1,26 @@
+const choices = ["ROCK", "PAPER", "SCISSORS"];
 //Generates a random up to max
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 function getComputerChoice() {
-  choice = ["ROCK", "PAPER", "SCISSORS"];
-  i = getRandomInt(3);
-  return choice[i];
+  const choice = getRandomInt(3);
+  return choices[choice];
+}
+
+function getHumanChoice() {
+  while (true) {
+    let playerChoice = prompt("Please enter a choice: Rock, Paper, Scissors ");
+    playerChoice = playerChoice.toUpperCase();
+
+    if (choices.includes(playerChoice)) {
+      return playerChoice;
+    } else {
+      alert("Invalid choice, pick again.");
+    }
+  }
 }
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
