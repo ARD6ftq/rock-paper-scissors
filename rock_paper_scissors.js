cd0fb9dcf9bec0431e40061716fef0b7987029d5
Tony <anthony.duncan117@gmail.com>
2024-06-14 17:45:46 -0400

    Play round function returns winner

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,15 +1,18 @@
 const choices = ["ROCK", "PAPER", "SCISSORS"];
+let playerScore = 0;
+let cpuScore = 0;
+
 //Generates a random up to max
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function getComputerChoice() {
+function getCPUChoice(choices) {
   const choice = getRandomInt(3);
   return choices[choice];
 }
 
-function getHumanChoice() {
+function getHumanChoice(choices) {
   while (true) {
     let playerChoice = prompt("Please enter a choice: Rock, Paper, Scissors ");
     playerChoice = playerChoice.toUpperCase();
@@ -22,5 +25,21 @@ function getHumanChoice() {
   }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound (playerChoice, cpuChoice) {
+    if (playerChoice == cpuChoice) {
+      return prompt("It's a tie!!");
+    } else if ((playerChoice == choices[0] && cpuChoice == choices[2]) || (playerChoice == choices[1] && cpuChoice == choices[0]) 
+    || (playerChoice == choices[2] && cpuChoice == choices[1])) {
+        return prompt ("You win!!");
+    } else {
+        return prompt ("CPU won! You lose!");
+    }
+}
+
+const playerChoice = getHumanChoice(choices);
+const cpuChoice = getCPUChoice(choices);
+playRound (playerChoice, cpuChoice);
+
+// function playGame () {
+    
+// }
